3f4fa74a96c9b9b192353cb0199d3304d3facff5
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2023-06-09 17:47:56 -0500

    first commit

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Zane Bartlett",
+	url: "https://example.com/",
+	language: "en",
+	description: "A place for my thoughts and work",
+	author: {
+		name: "Zane Bartlett",
+		email: "zanebartlet1@gmail.com",
+		url: "https://example.com/about-me/"
+	}
+}
